e8916eaf61b6a3ca25f55c72041b88a6f96d0971
Stela Capsa <stela.capsa@gmail.com>
2020-01-05 16:42:38 +0000

    Automatically backed up by Learn

---
+++ lib/post.rb
@@ -0,0 +1,7 @@
+class Post 
+  attr_accessor :title, :name, :author 
+  def initializ(name, author)
+    @name = name 
+    @author = author
+  end
+end 
---
+++ lib/song.rb
@@ -0,0 +1,7 @@
+class Song 
+  attr_accessor :title, :artist, :name
+  def initializ(name, artist)
+    @name = name 
+    @artist = artist 
+  end 
+end 
