8ff1ea74b7ef8f9be4376e35b2c7bf8eeb82eff1
Stela Capsa <stela.capsa@gmail.com>
2020-01-05 16:50:52 +0000

    Automatically backed up by Learn

--- lib/post.rb
+++ lib/post.rb
@@ -1,7 +1,7 @@
 class Post 
   attr_accessor :title, :name, :author 
-  def post_(name, author)
-    @name = name 
-    @author = author
-  end
+  # def post_(name, author)
+  #   @name = name 
+  #   @author = author
+  # end
 end 
